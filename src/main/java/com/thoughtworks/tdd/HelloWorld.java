284ce078d55c9d2002a572667ca014392084ee3d
lw <623656827@qq.com>
2019-08-23 16:43:45 +0800

    啦啦啦啦

--- src/main/java/com/thoughtworks/tdd/HelloWorld.java
+++ src/main/java/com/thoughtworks/tdd/HelloWorld.java
@@ -4,13 +4,24 @@ package com.thoughtworks.tdd;
  * Created by twer on 3/28/14.
  */
 public class HelloWorld {
-    private Dependency dependency;
+	public String test(int i) {
+		String result = "";
+		if(i<1||i>120) {
+			return "error";
+		}
+		if (i % 3 == 0) {
+			result += "Fizz";
+		}
+		if (i % 5 == 0) {
+			result += "Buzz";
+		}
+		if (i % 7 == 0) {
+			result += "Whizz";
+		}
+		if("".equals(result)) {
+			result=String.valueOf(i);
+		}
+		return result;
+	}
 
-    public HelloWorld(Dependency dependency) {
-
-        this.dependency = dependency;
-    }
-    public String beenCalled() {
-        return dependency.say();
-    }
 }
--- src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
+++ src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
@@ -1,54 +1,84 @@
 package com.thoughtworks.tdd;
 
-import com.thoughtworks.tdd.Dependency;
 import com.thoughtworks.tdd.HelloWorld;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 
 public class HelloWorldTest {
 
-    @BeforeAll
-    static void initTestEnv() {
-    }
+	@BeforeAll
+	static void initTestEnv() {
+		
+	}
 
-    @BeforeEach
-    void initEveryMethod() {
-    }
+	@BeforeEach
+	void initEveryMethod() {
+		
+	}
 
+	@Test
+	public void i_need_1() {
+		// given
+		int i = 1;
+		HelloWorld helloWorld = new HelloWorld();
+		// when
+		String result = helloWorld.test(i);
+		// then
+		assertThat(result, is("1"));
+	}
+	@Test
+	public void i_need_Fizz() {
+		// given
+		int i = 3;
+		HelloWorld helloWorld = new HelloWorld();
+		// when
+		String result = helloWorld.test(i);
+		// then
+		assertThat(result, is("Fizz"));
+	}
+	@Test
+	public void i_need_Buzz() {
+		// given
+		int i = 5;
+		HelloWorld helloWorld = new HelloWorld();
+		// when
+		String result = helloWorld.test(i);
+		// then
+		assertThat(result, is("Buzz"));
+	}
+	@Test
+	public void i_need_Whizz() {
+		// given
+		int i = 7;
+		HelloWorld helloWorld = new HelloWorld();
+		// when
+		String result = helloWorld.test(i);
+		// then
+		assertThat(result, is("Whizz"));
+	}
+	@Test
+	public void i_need_FizzWhizz() {
+		// given
+		int i = 21;
+		HelloWorld helloWorld = new HelloWorld();
+		// when
+		String result = helloWorld.test(i);
+		// then
+		assertThat(result, is("FizzWhizz"));
+	}
+	@Test
+	public void i_need_FizzBuzzWhizz() {
+		// given
+		int i = 105;
+		HelloWorld helloWorld = new HelloWorld();
+		// when
+		String result = helloWorld.test(i);
+		// then
+		assertThat(result, is("FizzBuzzWhizz"));
+	}
 
-    @Test
-    @DisplayName("try display name")
-    public void hello_world_test() {
-        //given
-        Dependency dependency = new Dependency();
-        HelloWorld helloWorld = new HelloWorld(dependency);
-
-        //when
-        String actual = helloWorld.beenCalled();
-
-        //then
-        assertThat(actual, is("Leave me alone."));
-    }
-
-    @Test
-    public void should_be_mocked() {
-        //given
-        Dependency dependency = mock(Dependency.class);
-        when(dependency.say()).thenReturn("Hello World");
-        HelloWorld helloWorld = new HelloWorld(dependency);
-
-        //when
-        String actual = helloWorld.beenCalled();
-
-        //then
-        assertThat(actual, is("Hello World"));
-    }
 }
